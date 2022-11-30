72f8ae3735b0e9961ed0e27114af1c391e492602
Sam Black <sam.black@lubbockisd.org>
2022-11-30 11:23:14 -0600

    updated for this year and made ready to go

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import static java.lang.System.out;
 public class Main {
 
     public static void main(String[] args) throws Throwable {
-        // write your code here
+
         partOne();
         partTwo();
 
