e5d22f1833d7bdcd6e7729641cac1373a1e691ea
Sam Black <sam.black@lubbockisd.org>
2022-12-01 12:26:40 -0600

    Solved!!

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 import static java.lang.System.out;
 
@@ -19,18 +16,50 @@ public class Main {
     private static void partTwo() throws Throwable {
         Scanner infile = new Scanner(new File("input.dat"));
 
+      
+        ArrayList<Long> elves = new ArrayList<>();
+        long sum = 0L;
+        while(infile.hasNext()){
+            String next = infile.nextLine();
+            if(next.length()==0){
+                elves.add(sum);
+                sum = 0L;
+            }
+            else {
+                sum += Long.parseLong(next);
+            }
 
-
+        }
+        if(sum != 0L)
+            elves.add(sum);
+        Collections.sort(elves);
+        Collections.reverse(elves);
+        long ans = elves.get(0) + elves.get(1) + elves.get(2);
+        out.println(ans);
     }
 
     private static void partOne() throws Throwable {
         Scanner infile = new Scanner(new File("test.dat"));
 
-        long ans = 0L;
-
+       
+        ArrayList<Long> elves = new ArrayList<>();
+        long sum = 0L;
         while(infile.hasNext()){
-            out.println();
+            String next = infile.nextLine();
+            if(next.length()==0){
+                elves.add(sum);
+                sum = 0L;
+            }
+            else {
+                sum += Long.parseLong(next);
+            }
+
         }
+        if(sum != 0L)
+            elves.add(sum);
+        Collections.sort(elves);
+        Collections.reverse(elves);
+        Long ans = elves.get(0);
         out.println(ans);
 
 
